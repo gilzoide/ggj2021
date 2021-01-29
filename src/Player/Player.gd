39d1c958ba1d4e829ad7156d7621e15a20b5c088
Cronus <cronuswaters@gmail.com>
2021-01-28 23:17:24 -0300

    Física básica adicionada (gravidade e objetos para colisão)

--- src/Player/Player.gd
+++ src/Player/Player.gd
@@ -2,28 +2,39 @@ extends KinematicBody
 
 export(float) var speed = 5
 export(float) var rotate_speed = 45
+export var gravity = Vector3.DOWN * 10
+var velocity = Vector3.ZERO
 
 onready var animation_player = $AnimationPlayer
 
 func _ready() -> void:
-    set_physics_process(true)
+	set_physics_process(true)
 
 func _physics_process(delta: float) -> void:
-    var delta_rotation: float = 0
-    if Input.is_action_pressed("move_left"):
-        delta_rotation += rotate_speed
-    if Input.is_action_pressed("move_right"):
-        delta_rotation -= rotate_speed
-    if delta_rotation:
-        rotate_y(deg2rad(delta_rotation * delta))
+	velocity += gravity * delta
+	get_input(delta)
+	velocity = move_and_slide(velocity, Vector3.UP)
 
-    var movement: float = 0
-    if Input.is_action_pressed("move_front"):
-        movement += 1
-    if Input.is_action_pressed("move_back"):
-        movement -= 1
-    if movement:
-        move_and_slide(transform.basis.z * movement * speed, Vector3.UP, true)
+func get_input(delta):
+	var delta_rotation: float = 0
+	var vy = velocity.y
+	velocity = Vector3.ZERO
+	if Input.is_action_pressed("move_left"):
+		delta_rotation += rotate_speed
+	if Input.is_action_pressed("move_right"):
+		delta_rotation -= rotate_speed
+	if delta_rotation:
+		rotate_y(deg2rad(delta_rotation * delta))
 
-    if movement:
-        animation_player.play("Moving")
+	var movement: float = 0
+	if Input.is_action_pressed("move_front"):
+		velocity += transform.basis.z * speed
+		movement += 1
+	if Input.is_action_pressed("move_back"):
+		velocity += -transform.basis.z * speed
+		movement -= 1
+	
+	velocity.y = vy
+
+	if movement:
+		animation_player.play("Moving")
