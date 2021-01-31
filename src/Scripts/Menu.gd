eef523b6f671879ac784c2b2ad18c053703523ca
gilzoide <gilzoide@gmail.com>
2021-01-31 17:38:30 -0300

    Add menu viaja ilha central

--- src/Scripts/Menu.gd
+++ src/Scripts/Menu.gd
@@ -1,33 +1,21 @@
 extends WindowDialog
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
 func go_to_ilha1():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-	get_tree().change_scene("res://Ilhas/IlhaCaminho/IlhaCaminho.tscn")
-	pass
+	PlayerVars.viaja_ilha(PlayerVars.Ilha.IlhaCaminho)
 
 func go_to_ilha2():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-	get_tree().change_scene("res://Ilhas/IlhaEscura/IlhaEscura.tscn")
-	pass
+	PlayerVars.viaja_ilha(PlayerVars.Ilha.IlhaEscura)
 
 func go_to_ilha3():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-	get_tree().change_scene("res://Ilhas/IlhaEstatuas/IlhaEstatuas.tscn")
-	pass
-	
+	PlayerVars.viaja_ilha(PlayerVars.Ilha.IlhaEstatua)
+
+func go_to_ilha_central():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	PlayerVars.viaja_ilha(PlayerVars.Ilha.IlhaCentral)
+
 func open_popup():
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	popup()
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
